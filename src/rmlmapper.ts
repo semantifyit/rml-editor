89284aed5ccf15d5c513db33249af98fbce9790f
Thibault Gerrier <thibault.gerrier@sti2.at>
2019-09-09 11:24:22 +0200

    can use brackets in xpath, regex fixes

--- src/rmlmapper.ts
+++ src/rmlmapper.ts
@@ -49,19 +49,19 @@ export const runRmlMapping = async (
 export const yarrrmlExtend = (yarrrml: string): string => {
   // replace function
   let str = yarrrml.replace(
-    /((?:parameters|pms): *\[)([\w@\^\.\/\$\(\)\"\' ,\[\]\|]+)(\])/g,
+    /((?:parameters|pms): *\[)([\w@\^\.\/\$\(\)\"\' ,\[\]\|\=]+)(\])/g,
     (...e) => {
       const [, cg1, cg2, cg3] = e as [string, string, string, string];
       const params = cg2
         .split(',')
-        .map((e, i) => `[schema:str${i}, ${e.trim()}]`)
+        .map((el, i) => `[schema:str${i}, ${el.trim()}]`)
         .join(', ');
       return cg1 + params + cg3;
     },
   );
   // replace join
   str = str.replace(
-    /join: *\[ *"?([\w@\^\.\/\$\:\-\*\,\ \']+)"? *, *"?([\w@\^\.\/\$\:\-\*\,\ \']+)"? *\]/g,
+    /join: *\[ *"?([\w@\^\.\/\$\:\-\*\,\ \'\)\()]+)"? *, *"?([\w@\^\.\/\$\:\-\*\,\ \'\(\)]+)"? *\]/g,
     'condition:{function:equal,parameters:[[str1,"$($1)"],[str2,"$($2)"]]}',
   );
   return str;
@@ -75,6 +75,57 @@ const escapeTable = {
   '}': '\\$RCB',
 };
 
+const yarrrmlEncodeBrackets = (str: string) => {
+  let level = 0;
+  let ret = '';
+
+  for(let i = 0; i< str.length; i++) {
+    const c = str[i];
+
+    if(level < 0) {
+      throw new Error('failed parsing brackets');
+    }
+
+    if (level === 0) {
+      switch (c) {
+        case '$': 
+          if (str[i+1] === '(') {
+            level ++;
+            i ++;
+            ret += '$('
+          }
+          break;
+        case '(': 
+        case ')':
+        default: 
+          ret += c;
+      }
+    } else {
+      switch (c) {
+        case '(': 
+          level++;
+          ret += '$LBR';
+          break;
+        case ')':
+          level--;
+          if (level === 0) {
+            ret += ')';
+          } else {
+            ret += '$RBR';
+          }
+          break;
+        default: 
+          ret += c;
+      }
+    }
+
+  }
+  return ret;
+}
+
+// console.log(yarrrmlEncodeBrackets('asd$(fii) fds $(fs(name(), conc(foo, my, "fellow"))g) why'));
+
+
 export const decodeRMLReplacements = (rml: string) =>
   Object.entries(escapeTable).reduce(
     (str, [char, code]) => str.replace(new RegExp(code, 'g'), char),
@@ -82,7 +133,8 @@ export const decodeRMLReplacements = (rml: string) =>
   );
 
 export const yarrrmlPlusToRml = async (yarrrml: string): Promise<string> => {
-  let mappingStr = yarrrmlExtend(yarrrml);  
+  let mappingStr = yarrrmlExtend(yarrrml);    
+  mappingStr = yarrrmlEncodeBrackets(mappingStr);
   mappingStr = await yarrrmlParse(mappingStr);
   mappingStr = decodeRMLReplacements(mappingStr);
   return mappingStr;
